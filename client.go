a90cf4d39743fe876c3ec9fc55cee5639902cd19
Stephan Zeissler <stephan.zeissler@adcloud.com>
2013-03-17 15:55:18 +0100

    go fmt

--- client.go
+++ client.go
@@ -1,9 +1,10 @@
 package epmd
 
 import (
-  "bytes"
-  "encoding/binary"
-  "net"
+	"bytes"
+	"encoding/binary"
+	"net"
+
 //  "log"
 )
 
@@ -12,108 +13,106 @@ var PORT_PLEASE2_REQ byte = byte('z')
 var PORT2_RESP = byte('w')
 
 const (
-  S_DEFAULT_PORT = "4369"
-  NL = byte(10)
-  SP = byte(32)
+	S_DEFAULT_PORT = "4369"
+	NL             = byte(10)
+	SP             = byte(32)
 )
 
 type Name struct {
-  Name string
-  Port string
+	Name string
+	Port string
 }
 
 type NodeInfo struct {
-  NodeName string
-  Port uint16
-  NodeType rune // H = Hidden or M
-  Protocol byte
-  HighestVersion uint16
-  LowestVersion uint16
-  Extra []byte
+	NodeName       string
+	Port           uint16
+	NodeType       rune // H = Hidden or M
+	Protocol       byte
+	HighestVersion uint16
+	LowestVersion  uint16
+	Extra          []byte
 }
 
 type Client struct {
-  hostname string
+	hostname string
 }
 
 func (this *Client) Names() ([]Name, error) {
-  return Names(this.hostname)
+	return Names(this.hostname)
 }
 
 func (this *Client) Get(name string) (*NodeInfo, error) {
-  return Get(this.hostname, name)
+	return Get(this.hostname, name)
 }
-
 
 // Returns a client for the local running epmd
 func Local() (*Client, error) {
-  return NewClient("localhost")
+	return NewClient("localhost")
 }
 
 // Creates a epmd client connection.
 // 
 func NewClient(hostname string) (*Client, error) {
-  return &Client{hostname}, nil
+	return &Client{hostname}, nil
 }
 
-
 func Names(epmd_hostname string) ([]Name, error) {
-  // Connect
-  laddr := net.JoinHostPort(epmd_hostname, S_DEFAULT_PORT)
-  conn, err := net.Dial("tcp", laddr)
-  if err != nil {
-    return nil, err
-  }
-  defer conn.Close()
+	// Connect
+	laddr := net.JoinHostPort(epmd_hostname, S_DEFAULT_PORT)
+	conn, err := net.Dial("tcp", laddr)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
 
-  // Send Request
-  n, err := sendRequest(conn, NAMES_REQ, nil)
-  if err != nil {
-    return nil, err
-  }
+	// Send Request
+	n, err := sendRequest(conn, NAMES_REQ, nil)
+	if err != nil {
+		return nil, err
+	}
 
-  // Read Response
-  var buffer []byte = make([]byte, 1024 * 1024)
-  n, err = conn.Read(buffer)
-  if err != nil {
-    return nil, err
-  }
+	// Read Response
+	var buffer []byte = make([]byte, 1024*1024)
+	n, err = conn.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
 
-  // Parse
-  return parseNamesResponse(buffer[0:n])
+	// Parse
+	return parseNamesResponse(buffer[0:n])
 }
 
 func Get(epmd_hostname, nodeName string) (*NodeInfo, error) {
-  // Connect
-  laddr := net.JoinHostPort(epmd_hostname, S_DEFAULT_PORT)
-  conn, err := net.Dial("tcp", laddr)
-  if err != nil {
-    return nil, err
-  }
-  defer conn.Close()
+	// Connect
+	laddr := net.JoinHostPort(epmd_hostname, S_DEFAULT_PORT)
+	conn, err := net.Dial("tcp", laddr)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
 
-  // Request
-  n, err := sendRequest(conn, PORT_PLEASE2_REQ, []byte(nodeName))
-  if err != nil {
-    return nil, err
-  }
+	// Request
+	n, err := sendRequest(conn, PORT_PLEASE2_REQ, []byte(nodeName))
+	if err != nil {
+		return nil, err
+	}
 
-  // Read Response
-  var buffer []byte = make([]byte, 1024 * 1024)
-  n, err = conn.Read(buffer)
-  if err != nil {
-    return nil, err
-  }
+	// Read Response
+	var buffer []byte = make([]byte, 1024*1024)
+	n, err = conn.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
 
-  // Parsing
-  return parsePort2PleaseResponse(buffer[0:n])
+	// Parsing
+	return parsePort2PleaseResponse(buffer[0:n])
 }
 
 func sendRequest(conn net.Conn, reqId byte, data []byte) (int, error) {
-  var req []byte = make([]byte, 3)
-  binary.BigEndian.PutUint16(req[0:2], uint16(1 + len(data)))
-  req[2] = reqId
-  req = bytes.Join([][]byte{req, data}, []byte{})
-  // log.Printf("REQ: %v\n", req)
-  return conn.Write(req)
+	var req []byte = make([]byte, 3)
+	binary.BigEndian.PutUint16(req[0:2], uint16(1+len(data)))
+	req[2] = reqId
+	req = bytes.Join([][]byte{req, data}, []byte{})
+	// log.Printf("REQ: %v\n", req)
+	return conn.Write(req)
 }
--- parser.go
+++ parser.go
@@ -1,10 +1,10 @@
 package epmd
 
 import (
-  "bytes"
-  "encoding/binary"
-//  "log"
-  "fmt"
+	"bytes"
+	"encoding/binary"
+	//  "log"
+	"fmt"
 )
 
 // [119       # Response code (ascii)
@@ -19,31 +19,30 @@ import (
 //   0        # Length of extra field
 //   0]       # ?????????????
 func parsePort2PleaseResponse(data []byte) (*NodeInfo, error) {
-  if data[0] != PORT2_RESP {
-    return nil, fmt.Errorf("Expected response code PORT2_RESP")
-  }
-  if data[1] != 0 {
-    return nil, fmt.Errorf("Epmd responded with error code %v", int(data[1]))
-  }
-  
-  // log.Printf("RES: %v\n", data)
-  var nodeInfo NodeInfo
-  nodeInfo.Port = binary.BigEndian.Uint16(data[2:4])
-  nodeInfo.NodeType = rune(data[4])
-  nodeInfo.Protocol = data[5]
-  nodeInfo.HighestVersion = binary.BigEndian.Uint16(data[6:8])
-  nodeInfo.LowestVersion = binary.BigEndian.Uint16(data[8:10])
+	if data[0] != PORT2_RESP {
+		return nil, fmt.Errorf("Expected response code PORT2_RESP")
+	}
+	if data[1] != 0 {
+		return nil, fmt.Errorf("Epmd responded with error code %v", int(data[1]))
+	}
 
-  nameLen := binary.BigEndian.Uint16(data[10:12])
-  nodeInfo.NodeName = string(data[12:12 + nameLen])
+	// log.Printf("RES: %v\n", data)
+	var nodeInfo NodeInfo
+	nodeInfo.Port = binary.BigEndian.Uint16(data[2:4])
+	nodeInfo.NodeType = rune(data[4])
+	nodeInfo.Protocol = data[5]
+	nodeInfo.HighestVersion = binary.BigEndian.Uint16(data[6:8])
+	nodeInfo.LowestVersion = binary.BigEndian.Uint16(data[8:10])
 
-  data = data[12 + nameLen:]
-  extraLen := binary.BigEndian.Uint16(data[0:2])
-  nodeInfo.Extra = data[2:2+extraLen]
+	nameLen := binary.BigEndian.Uint16(data[10:12])
+	nodeInfo.NodeName = string(data[12 : 12+nameLen])
 
-  return &nodeInfo, nil
+	data = data[12+nameLen:]
+	extraLen := binary.BigEndian.Uint16(data[0:2])
+	nodeInfo.Extra = data[2 : 2+extraLen]
+
+	return &nodeInfo, nil
 }
-
 
 // [0 0 17 17  # EPMD Port
 //
@@ -53,20 +52,20 @@ func parsePort2PleaseResponse(data []byte) (*NodeInfo, error) {
 //  110 97 109 101 32 114 97 98 98 105 116 32 97 116 32 112 111 114 116 32 53 54 54 54 50 10
 //  110 97 109 101 32 114 105 97 107 32 97 116 32 112 111 114 116 32 52 57 50 50 51 10
 // ]
-func parseNamesResponse(data []byte) ([]Name, error){
-  data = data[4:] // Skip the first four bytes (which is the epmd port)
+func parseNamesResponse(data []byte) ([]Name, error) {
+	data = data[4:] // Skip the first four bytes (which is the epmd port)
 
-  bnodes := bytes.Split(data, []byte{NL})
-  nodes := make([]Name, len(bnodes)-1)
+	bnodes := bytes.Split(data, []byte{NL})
+	nodes := make([]Name, len(bnodes)-1)
 
-  for i, bnode := range bnodes {
-    if len(bnode) == 0 {
-      break
-    }
-    bfields := bytes.Split(bnode, []byte{SP})
+	for i, bnode := range bnodes {
+		if len(bnode) == 0 {
+			break
+		}
+		bfields := bytes.Split(bnode, []byte{SP})
 
-    nodes[i].Name = string(bfields[1])
-    nodes[i].Port = string(bfields[4])
-  }
-  return nodes, nil
+		nodes[i].Name = string(bfields[1])
+		nodes[i].Port = string(bfields[4])
+	}
+	return nodes, nil
 }
